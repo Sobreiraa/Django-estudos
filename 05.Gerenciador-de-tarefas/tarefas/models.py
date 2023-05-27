1f82cc05f57bf790db8c916d2799daea3cd5abae
Matheus Sobreira <matheussobreira2702@gmail.com>
2023-05-27 20:07:39 -0300

    Gerenciando tarefas

---
+++ 05.Gerenciador-de-tarefas/tarefas/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+class Tarefa(models.Model):
+    OPCOES_CATEGORIA = (
+        ('urgente', 'Urgente'),
+        ('importante', 'Importante'),
+        ('precisa ser feito', 'Precisa ser feito'),
+    )
+
+    OPCOES_STATUS = (
+        ('concluído', 'Concluído'),
+        ('pendente', 'Pendente'),
+        ('adiado', 'Adiado'),
+    )
+
+    descricao = models.TextField(max_length=400)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+    categoria = models.CharField(max_length=30, choices=OPCOES_CATEGORIA, default='importante')
+    status = models.CharField(max_length=30, choices=OPCOES_STATUS, default='pendente')
---
+++ 05.Gerenciador-de-tarefas/tarefas/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from .models import Tarefa
+
+def tarefas_lista(request):
+    tarefas_pendentes = Tarefa.objects.filter(status='pendente')
+    return render(request, 'tarefas/tarefas_pendentes.html', {'tarefas_pendentes': tarefas_pendentes})
