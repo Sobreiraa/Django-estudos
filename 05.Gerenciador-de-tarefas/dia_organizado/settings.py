cd5995c245878fa0328e03a403a761c79c1c523f
Matheus Sobreira <matheussobreira2702@gmail.com>
2023-05-29 16:22:03 -0300

    atualizando settings

--- 05.Gerenciador-de-tarefas/dia_organizado/settings.py
+++ 05.Gerenciador-de-tarefas/dia_organizado/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jt4072l_q9%9hua#-mkv)mp$bp!^tm904*fbbx#ah*!6bycg+&'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
