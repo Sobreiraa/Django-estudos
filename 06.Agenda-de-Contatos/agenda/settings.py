d3a79f78e7bc3e2566ad7bbfd45f33939c4ceb9c
Matheus Sobreira <matheussobreira2702@gmail.com>
2023-05-29 16:24:15 -0300

    atualizando o settings.py

--- 06.Agenda-de-Contatos/agenda/settings.py
+++ 06.Agenda-de-Contatos/agenda/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -9,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i*xb2el7eg*(*krqu+lrtg0%gn&k=q)ko3o7n-()f2c&ciril%'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
