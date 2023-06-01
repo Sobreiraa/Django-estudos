a132aa6fe804a1f0ce2d2307abaa437f0b49952c
Matheus Sobreira <matheussobreira2702@gmail.com>
2023-06-01 18:09:25 -0300

    removendo agenda

--- 06.Agenda-de-Contatos/contatos/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Contato
-
-def contatos(request):
-    contatos = Contato.objects.all()
-    return render(request, 'contatos/contatos.html', {'contatos': contatos})
