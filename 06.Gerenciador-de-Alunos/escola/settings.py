9891f7e723b75ffee81117f005fb6dc91c8c99db
Matheus Sobreira <matheussobreira2702@gmail.com>
2023-06-04 16:36:35 -0300

    .env

--- 06.Gerenciador-de-Alunos/escola/settings.py
+++ 06.Gerenciador-de-Alunos/escola/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+from decouple import config
 import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m22&f)wsd9sds$w0tb(x@w&^#jh4l$4_+3qe^*n5mgn9pn$cbg'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
